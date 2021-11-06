af2babd3b78353dae453adb4ff9e52573592f6a1
is0383kk <is0383kk@gmail.com>
2021-11-06 21:37:06 +0900

    updated

---
+++ debug_program/custom_data.py
@@ -0,0 +1,219 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt 
+
+root = "/home/is0383kk/workspace/mnist_png"
+
+class CustomDataset(torch.utils.data.Dataset):
+    classes = ["0","1","2","3","4","5","6","7","8","9"]
+    
+    def __init__(self, root, transform = None, train = True):
+        self.transform = transform # 前処理クラス
+        # 画像ラベルの一覧を保持するリスト
+        self.images = []
+        self.labels = []
+        # データセット読み込み先パス
+        root = "/home/is0383kk/workspace/mnist_png"
+        # trainとval用で分ける
+        if train == True:
+            mnist_0_path = os.path.join(root, "training500", "0")
+            mnist_1_path = os.path.join(root, "training500", "1")
+            mnist_2_path = os.path.join(root, "training500", "2")
+            mnist_3_path = os.path.join(root, "training500", "3")
+            mnist_4_path = os.path.join(root, "training500", "4")
+            mnist_5_path = os.path.join(root, "training500", "5")
+            mnist_6_path = os.path.join(root, "training500", "6")
+            mnist_7_path = os.path.join(root, "training500", "7")
+            mnist_8_path = os.path.join(root, "training500", "8")
+            mnist_9_path = os.path.join(root, "training500", "9")
+        else:
+            mnist_0_path = os.path.join(root, "all", "0")
+            mnist_1_path = os.path.join(root, "all", "1")
+        
+        # 数字ごとの画像データ一覧を取得
+        mnist_image_0 = os.listdir(mnist_0_path)
+        mnist_image_1 = os.listdir(mnist_1_path)
+        """mnist_image_2 = os.listdir(mnist_2_path)
+        mnist_image_3 = os.listdir(mnist_3_path)
+        mnist_image_4 = os.listdir(mnist_4_path)
+        mnist_image_5 = os.listdir(mnist_5_path)
+        mnist_image_6 = os.listdir(mnist_6_path)
+        mnist_image_7 = os.listdir(mnist_7_path)
+        mnist_image_8 = os.listdir(mnist_8_path)
+        mnist_image_9 = os.listdir(mnist_9_path)"""
+        # 画像データにラベル付け
+        mnist_label_0 = [0] * len(mnist_image_0)
+        mnist_label_1 = [1] * len(mnist_image_1)
+        """mnist_label_2 = [2] * len(mnist_image_2)
+        mnist_label_3 = [3] * len(mnist_image_3)
+        mnist_label_4 = [4] * len(mnist_image_4)
+        mnist_label_5 = [5] * len(mnist_image_5)
+        mnist_label_6 = [6] * len(mnist_image_6)
+        mnist_label_7 = [7] * len(mnist_image_7)
+        mnist_label_8 = [8] * len(mnist_image_8)
+        mnist_label_9 = [9] * len(mnist_image_9)"""
+
+        for image, label in zip(mnist_image_0, mnist_label_0):
+            self.images.append(os.path.join(mnist_0_path, image))
+            self.labels.append(label)
+        # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_1, mnist_label_1):
+            self.images.append(os.path.join(mnist_1_path, image))
+            self.labels.append(label)
+        # 1つのリスト構造にする 
+        """for image, label in zip(mnist_image_2, mnist_label_2):
+            self.images.append(os.path.join(mnist_2_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_3, mnist_label_3):
+            self.images.append(os.path.join(mnist_3_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_4, mnist_label_4):
+            self.images.append(os.path.join(mnist_4_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_5, mnist_label_5):
+            self.images.append(os.path.join(mnist_5_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_6, mnist_label_6):
+            self.images.append(os.path.join(mnist_6_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_7, mnist_label_7):
+            self.images.append(os.path.join(mnist_7_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_8, mnist_label_8):
+            self.images.append(os.path.join(mnist_8_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_9, mnist_label_9):
+            self.images.append(os.path.join(mnist_9_path, image))
+            self.labels.append(label)"""
+
+    def __getitem__(self, index):
+        # インデックスを元に画像のファイルパスとラベルを取得
+        image = self.images[index]
+        label = self.labels[index]
+        # 画像ファイルパスから画像を読み込む
+        with open(image, "rb") as f:
+            image = Image.open(f)
+            #image = image.resize((28, 28), Image.LANCZOS)
+            image = image.convert("L")
+        # 前処理
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        # 画像とラベルのペアを返す
+        return image, label
+
+    def __len__(self):
+        # データ数を指定
+        return len(self.images)
+
+class GenDataset(torch.utils.data.Dataset):
+    classes = ["0","1","2","3","4","5","6","7","8","9"]
+    
+    def __init__(self, root, transform = None, train = True):
+        self.transform = transform # 前処理クラス
+        # 画像ラベルの一覧を保持するリスト
+        self.images = []
+        self.labels = []
+        # データセット読み込み先パス
+        root = "/home/is0383kk/workspace/mnist_png"
+        # trainとval用で分ける
+        if train == True:
+            mnist_0_path = os.path.join(root, "all", "0")
+            mnist_1_path = os.path.join(root, "all", "1")
+            mnist_2_path = os.path.join(root, "all", "2")
+            mnist_3_path = os.path.join(root, "all", "3")
+            mnist_4_path = os.path.join(root, "all", "4")
+            mnist_5_path = os.path.join(root, "all", "5")
+            mnist_6_path = os.path.join(root, "all", "6")
+            mnist_7_path = os.path.join(root, "all", "7")
+            mnist_8_path = os.path.join(root, "all", "8")
+            mnist_9_path = os.path.join(root, "all", "9")
+        else:
+            mnist_0_path = os.path.join(root, "testing", "0")
+        
+        # 数字ごとの画像データ一覧を取得
+        mnist_image_0 = os.listdir(mnist_0_path)
+        mnist_image_1 = os.listdir(mnist_1_path)
+        mnist_image_2 = os.listdir(mnist_2_path)
+        mnist_image_3 = os.listdir(mnist_3_path)
+        mnist_image_4 = os.listdir(mnist_4_path)
+        mnist_image_5 = os.listdir(mnist_5_path)
+        mnist_image_6 = os.listdir(mnist_6_path)
+        mnist_image_7 = os.listdir(mnist_7_path)
+        mnist_image_8 = os.listdir(mnist_8_path)
+        mnist_image_9 = os.listdir(mnist_9_path)
+        # 画像データにラベル付け
+        mnist_label_0 = [0] * len(mnist_image_0)
+        mnist_label_1 = [1] * len(mnist_image_1)
+        mnist_label_2 = [2] * len(mnist_image_2)
+        mnist_label_3 = [3] * len(mnist_image_3)
+        mnist_label_4 = [4] * len(mnist_image_4)
+        mnist_label_5 = [5] * len(mnist_image_5)
+        mnist_label_6 = [6] * len(mnist_image_6)
+        mnist_label_7 = [7] * len(mnist_image_7)
+        mnist_label_8 = [8] * len(mnist_image_8)
+        mnist_label_9 = [9] * len(mnist_image_9)
+
+        for image, label in zip(mnist_image_0, mnist_label_0):
+            self.images.append(os.path.join(mnist_0_path, image))
+            self.labels.append(label)
+        # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_1, mnist_label_1):
+            self.images.append(os.path.join(mnist_1_path, image))
+            self.labels.append(label)
+        # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_2, mnist_label_2):
+            self.images.append(os.path.join(mnist_2_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_3, mnist_label_3):
+            self.images.append(os.path.join(mnist_3_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_4, mnist_label_4):
+            self.images.append(os.path.join(mnist_4_path, image))
+            self.labels.append(label)
+            # 1つのリスト構造にする 
+        for image, label in zip(mnist_image_5, mnist_label_5):
+            self.images.append(os.path.join(mnist_5_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_6, mnist_label_6):
+            self.images.append(os.path.join(mnist_6_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_7, mnist_label_7):
+            self.images.append(os.path.join(mnist_7_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_8, mnist_label_8):
+            self.images.append(os.path.join(mnist_8_path, image))
+            self.labels.append(label)
+        for image, label in zip(mnist_image_9, mnist_label_9):
+            self.images.append(os.path.join(mnist_9_path, image))
+            self.labels.append(label)
+
+    def __getitem__(self, index):
+        # インデックスを元に画像のファイルパスとラベルを取得
+        image = self.images[index]
+        label = self.labels[index]
+        # 画像ファイルパスから画像を読み込む
+        with open(image, "rb") as f:
+            image = Image.open(f)
+            #image = image.resize((28, 28), Image.LANCZOS)
+            image = image.convert("L")
+        # 前処理
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        # 画像とラベルのペアを返す
+        return image, label
+
+    def __len__(self):
+        # データ数を指定
+        return len(self.images)
---
+++ debug_program/show_image.py
@@ -0,0 +1,115 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+import os
+from PIL import Image
+import matplotlib.pyplot as plt 
+from custom_data import CustomDataset, GenDataset
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from torch.utils.data.dataset import Subset
+import matplotlib.animation as animation
+
+
+root = "/home/is0383kk/workspace/mnist_png"
+
+#前処理
+data_transforms = transforms.Compose([
+        #transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])
+    ])
+#前処理なし
+to_tensor = transforms.Compose([
+    transforms.ToTensor(),
+])
+pad1 = transforms.Compose([
+    transforms.Pad(padding=(0, -5, -10, 0), fill=0, padding_mode='constant'),transforms.Resize((28, 28)),# 左・上・右・下
+    transforms.Pad(padding=(0, 5, 10, 0), fill=0, padding_mode='constant'),transforms.Resize((28, 28)),
+    #transforms.CenterCrop(28),
+    transforms.ToTensor(),
+])
+pad2 = transforms.Compose([
+    transforms.Pad(padding=(-15, 0, 0, 0), fill=0, padding_mode='constant'),transforms.Resize((28, 28)),# 左・上・右・下
+    transforms.Pad(padding=(15, 0, 0, 0), fill=0, padding_mode='constant'),transforms.Resize((28, 28)),
+    #transforms.CenterCrop(28),
+    transforms.ToTensor(),
+])
+trans = transforms.Compose([
+    transforms.ToTensor(),
+])
+trans_ang1 = transforms.Compose([
+    transforms.RandomRotation(degrees=(-25,-25)),
+    transforms.ToTensor(),
+])
+trans_ang2 = transforms.Compose([
+    transforms.RandomRotation(degrees=(25,25)),
+    transforms.ToTensor(),
+])
+
+class ToNDarray(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        x_shape = x.shape    #x=(C,H,W)
+        x = x.detach().clone().cpu()   #x=(C,H,W)
+        x = x.numpy()   #x=(C,H,W)
+        if x_shape[0] == 1:       #C=1の時
+            x = x[0]    #x=(H,W)にする
+        else:
+            x = x.transpose(1,2,0)  #x=(H,W,C)にする
+        return x
+
+# データのプロット
+custom_dataset = GenDataset(root, trans_ang1, train=True)
+#custom_dataset = CustomDataset(root, data_transforms, train=True)
+print(f"データセット数 :{len(custom_dataset)}")
+batch_size = 20
+custom_loader = torch.utils.data.DataLoader(dataset=custom_dataset, batch_size=batch_size, shuffle=False)
+print("batch_size",batch_size)
+
+
+# データセット分割調整
+trainval_dataset = datasets.MNIST('./../../data', train=True, transform=pad1, download=False)
+n_samples = len(trainval_dataset) 
+#train_size = int(n_samples * 0.1) # 6000枚
+train_size = int(n_samples * 0.13) # 9000枚
+#train_size = int(n_samples * 0.25) # 15000枚
+#train_size = int(n_samples * 0.01) # 600枚
+print(f"Number of training datasets :{train_size}")
+subset1_indices = list(range(0,train_size)); subset2_indices = list(range(train_size,n_samples)) 
+train_dataset = Subset(trainval_dataset, subset1_indices); val_dataset = Subset(trainval_dataset, subset2_indices)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False)
+
+
+def show(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)), interpolation="nearest")
+    plt.show()
+
+for i, (images, labels) in enumerate(custom_loader):
+    print(f"i :{i}")
+    print(f"images :{images}, {images.size()}")
+    trans = ToNDarray()
+    print(f"labels :{labels}")
+    #print(images)
+    #print(labels.numpy())
+    #im = trans(images[0])
+    #print(f"im: {im}")
+    #transform = transforms.FiveCrop(24)
+    #imgs = transform(im)
+    images = torchvision.utils.make_grid(images, padding=1)
+    plt.imshow(np.transpose(images, (1,2,0)), interpolation="nearest")
+    plt.savefig('ang2_75.png')
+    
+    plt.show()
+    plt.close()
+    #show(torchvision.utils.make_grid(images, padding=1))
+    #plt.axis("off")
+
+    break
+
